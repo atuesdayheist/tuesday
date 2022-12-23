919e252b87ccd14792bfff7a78b8ca9138bfc0db
atuesdayheist <r@atuesday.io>
2022-12-23 15:15:29 -0500

    Fix prod/dev endpoint mixups

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -2,26 +2,28 @@ import axios from 'axios';
 
 axios.defaults.withCredentials = true;
 
-const api_endpoint = process.env.REACT_APP_SERVER_URL;
+let api_endpoint = '';
+if (process.env.NODE_ENV == 'development') {
+  api_endpoint = 'http://localhost.3000';
+} else {
+  api_endpoint = 'https://r.atuesday.io';
+}
 
 export async function onRegistration(registrationData) {
   return await axios.post(
-    `https://${api_endpoint}/api/register`,
+    `${api_endpoint}/api/register`,
     registrationData,
   );
 }
 
 export async function onLogin(loginData) {
-  return await axios.post(
-    `https://${api_endpoint}/api/login`,
-    loginData,
-  );
+  return await axios.post(`${api_endpoint}/api/login`, loginData);
 }
 
 export async function onLogout() {
-  return await axios.get(`https://${api_endpoint}/api/logout`);
+  return await axios.get(`${api_endpoint}/api/logout`);
 }
 
 export async function fetchProtectedInfo() {
-  return await axios.get(`https://${api_endpoint}/api/protected`);
+  return await axios.get(`${api_endpoint}/api/protected`);
 }
